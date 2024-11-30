be09279b3f236e92ed76d699c40a741d364f20cb
IzzatjonAkbarov <akbrv.i.006@gmail.com>
2024-11-30 14:45:01 +0500

    last is dane

--- js/fourth.js
+++ js/fourth.js
@@ -1,8 +1,9 @@
+const { string } = require("postcss-selector-parser");
+
 let str1 = "abc";
 let str2 = "";
 for (i = 0; i < str1.length; i++) {
-  str2 = str1[i];
+  str2 += String.fromCharCode(str1[i].charCodeAt() + 1);
 }
-str2.charCodeAt(99);
 console.log(str2);
 // algorithmga tushunmadim
