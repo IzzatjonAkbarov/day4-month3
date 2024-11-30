92b538c92e30b9d90fd8f22757c604cf00c3a1a3
IzzatjonAkbarov <akbrv.i.006@gmail.com>
2024-11-30 11:02:12 +0500

    task is done with all swiper and responsive

---
+++ main.js
@@ -0,0 +1,45 @@
+// let color = "white";
+// switch (color) {
+//   case "red":
+//     // console.log("true");
+//     break;
+//   case "blue":
+//     // console.log("false");
+//     break;
+//   case "orange":
+//     // console.log("false");
+//     break;
+//   case "green":
+//     // console.log("false");
+//     break;
+//   case "black":
+//     // console.log("false");
+//     break;
+//   case "white":
+//     // console.log("false");
+//     break;
+//   default:
+//     // console.log("error");
+//     break;
+// }
+// i=0;
+// do{
+// i<10;
+// }
+// let str = "Hello WorlD";
+// let all = "";
+// for (i = 0; i < str.length; i++) {
+//   finalresult = str[i];
+//   if (finalresult == finalresult.toUpperCase()) {
+//     all += finalresult;
+//   }
+// }
+// console.log(all);
+let str = "123678";
+
+let middle;
+let finalresult = "";
+for (i = 1; i < str.length; i++) {
+  middle += str[i];
+}
+console.log(middle);
